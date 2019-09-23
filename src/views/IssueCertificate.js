b7e13cc37c3874c8752ae65e410566a3aa7075e3
Hasan <hasanrasheed921@gmail.com>
2019-09-23 18:47:58 +0500

    issue certificate icons added

--- src/views/IssueCertificate.js
+++ src/views/IssueCertificate.js
@@ -30,6 +30,44 @@ import ReactFileReader from 'react-file-reader';
 import MaterialTable from 'material-table'
 import certificate from '../images/certificate.png'
 import { generateQrCodes } from '../utils/functions'
+import { forwardRef } from 'react';
+
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowUpward from '@material-ui/icons/ArrowUpward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} fontSize = "large" ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowUpward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+  };
+
 const csv = require('csv-parser')
 const fs = require('fs')
 const results = [];
@@ -141,6 +179,7 @@ class IssueCertificate extends Component {
     //     // handle error
     //     console.log(error);
     //   })
+    
     if (this.props.selectedInstituteName.name != "Select Organization") {
       console.log("inside if")
       axios.get(Routes.CLASSIFICATION + this.props.selectedInstituteName.id)
@@ -391,7 +430,9 @@ class IssueCertificate extends Component {
       selectedClassification: this.state.registeredClassifications[ev.target.value]
     })
     console.log(this.state.registeredClassifications[ev.target.value].list)
+    if(ev.target.value !=0){
     this.getColumnsFromClassification(this.state.registeredClassifications[ev.target.value].list)
+    }
     // this.setState({category: this.state.registeredClassifications[e.target.value].obj})
     // this.setState({
     //   category: ev.target.value
@@ -479,6 +520,7 @@ console.log(this.state)
           <div style={{ marginBottom: "2em" }}>
             <MaterialTable
               title="Recievers List"
+              icons={tableIcons}
               columns={this.state.columns}
               data={this.state.data}
               editable={{
